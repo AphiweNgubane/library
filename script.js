ddda207020d23668b721b162d7b370016ba94259
AphiweNgubane <164533620+AphiweNgubane@users.noreply.github.com>
2025-03-16 21:06:53 +0200

    Add book display function with dynamic UI updates

--- script.js
+++ script.js
@@ -15,7 +15,26 @@ function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 
-    return newBook;
+    displayBooks();
+}
+
+function displayBooks() {
+  const librayContainer = document.getElementById("library-container");
+
+  librayContainer.innerHTML = ""
+
+  myLibrary.forEach((book) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+
+    bookCard.innerHTML = `
+        <h2>${book.title}</h2>
+        <p><strong>Author:</strong> ${book.author}</p>
+        <p><strong>Pages:</strong> ${book.pages}</p>
+        <p><strong>Read:</strong> ${book.read ? "Yes" : "No"}</p>
+        `;
+    librayContainer.appendChild(bookCard);
+  });
 }
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
